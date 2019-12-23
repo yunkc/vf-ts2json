aa6568d2e1f42460020510d11b6d2458f4d83222
haojen ma <ehazomm@gmail.com>
2019-12-23 21:48:23 +0800

    use strict ts mode

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import global from './global'
 import components from './components'
 import {IVFTemplate, LoadMode, ScaleMode} from "../types/IVFTemplate";
 
-const main: IVFTemplate = {
+const Main: IVFTemplate = {
     assets,
     global,
     scenes,
     width: 890,
     components,
-    height: 501,
+    height: 500,
     baseUrl: '',
     name: 'STC01',
     conversion: '',
@@ -19,4 +19,4 @@ const main: IVFTemplate = {
     scaleMode: ScaleMode.showAll
 }
 
-export default main
+export default Main;
