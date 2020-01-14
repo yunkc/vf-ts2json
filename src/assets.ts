3e7c7e329ddc2897b1a75554410ea381694d1d08
yangxiao3 <yangxiao3@vipkid.com.cn>
2020-01-14 19:14:54 +0800

    add index.html

--- src/assets.ts
+++ src/assets.ts
@@ -5,27 +5,27 @@ const assets = {
     [Store.titleBackground]: {
         type: Assets.type.image,
         name: Store.titleBackground.toString(),
-        url: 'title-background.png'
+        url: 'dino.png'
     },
     [Store.choiceButtonRight]: {
         type: Assets.type.image,
         name: Store.choiceButtonRight.toString(),
-        url: 'choice-static-success.png'
+        url: 'dino.png'
     },
     [Store.choiceButtonWrong]: {
         type: Assets.type.image,
         name: Store.choiceButtonWrong.toString(),
-        url: 'choice-static-wrong.png',
+        url: 'dino.png',
     },
     [Store.contentImgBorder]: {
         type: Assets.type.image,
         name: Store.contentImgBorder.toString(),
-        url: 'content-image-border.png'
+        url: 'dino.png'
     },
     [Store.contentQuestionImg]: {
         type: Assets.type.image,
         name: Store.contentQuestionImg.toString(),
-        url: 'content-question-img.png'
+        url: 'dino.png'
     }
 }
 
