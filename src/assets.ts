c5ed1ccfd31888bbdadaa7bd19662d65611d227c
haojen ma <ehazomm@gmail.com>
2020-05-09 16:44:24 +0800

    change assets AssetType use way

--- src/assets.ts
+++ src/assets.ts
@@ -3,7 +3,7 @@ import {Assets, AssetType} from '../types/IVFTemplate'
 
 const assets: Assets = {
     [Ids.dinoImage]: {
-        type: AssetType.IMAGE,
+        type: AssetType.image,
         url: './assets/dino.png',
         name: Ids.dinoImage.toString()
     },
